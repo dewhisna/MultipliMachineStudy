66b4b0c478850e070779b213e4fdc379ae4b00fa
Donald Whisnant <dewhisna@users.noreply.github.com>
2022-01-17 12:28:58 -0600

    Bump version to 2.2.0 for release.

--- src/MachineStudy/version.h
+++ src/MachineStudy/version.h
@@ -25,12 +25,12 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define VER_QT						"2.1.0"
-#define VER_FILEVERSION				2,1,0,0
-#define VER_FILEVERSION_STR			"2,1,0,0\0"
+#define VER_QT						"2.2.0"
+#define VER_FILEVERSION				2,2,0,0
+#define VER_FILEVERSION_STR			"2,2,0,0\0"
 
-#define VER_PRODUCTVERSION			2,1,0,0
-#define VER_PRODUCTVERSION_STR		"2,1,0,0\0"
+#define VER_PRODUCTVERSION			2,2,0,0
+#define VER_PRODUCTVERSION_STR		"2,2,0,0\0"
 
 #ifndef VER_SPECIALVERSION_STR
 #define VER_SPECIALVERSION_STR		"\0"
